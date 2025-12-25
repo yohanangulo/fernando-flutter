b2b4a09dc80a01d11c0e2bef260e9390c701069d
yohanangulo <anguloyohan98@gmail.com>
2025-12-25 16:26:42 -0400

    New screens created

---
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  const AppTutorialScreen({super.key});
+
+  static const String name = 'app_tutorial_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ widgets_app/lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiControlsScreen extends StatelessWidget {
+  const UiControlsScreen({super.key});
+
+  static const String name = 'ui_controls_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
