fefbca171479dd36e2fbfb5af11c654640cc532c
yohanangulo <anguloyohan98@gmail.com>
2026-01-02 13:19:12 -0400

    Fix invalid type in repository

--- cinemapedia/lib/infrastructure/repositories/movie_repository_impl.dart
+++ cinemapedia/lib/infrastructure/repositories/movie_repository_impl.dart
@@ -1,11 +1,10 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';
-import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
 
 class MovieRepositoryImpl implements MoviesRepository {
   MovieRepositoryImpl(this._moviedbDatasource);
 
-  final MoviedbDatasource _moviedbDatasource;
+  final MoviesDatasource _moviedbDatasource;
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) {
