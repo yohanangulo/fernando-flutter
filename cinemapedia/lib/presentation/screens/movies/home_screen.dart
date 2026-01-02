2079effc72398e9d9494846692cc2ef648b09b9f
yohanangulo <anguloyohan98@gmail.com>
2026-01-01 20:41:13 -0400

    Remove unused import

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/env.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
